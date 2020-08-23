c897c0ac43ac8c550c8a57c0b8e1b3307f0744e7
Shafeek <mohammedshafeek2112@gmail.com>
2020-08-23 14:33:58 +0800

    Setting Attribute

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,11 +7,17 @@ class Counter extends Component {
         count : 0
     }
 
+    // In the style property, should use the camelcase for attribute.
+    //Can apply style using two method, one is here or inline.
+    style = {
+        fontWeight : 'bold',
+    }
+
     render() {
         return (
             <React.Fragment>
-                <span>{this.countFormat()}</span>
-                <button>Increment</button>
+                <span style={this.style} className="badge badge-primary m-2" >{ this.countFormat() }</span> 
+                <button style={{ fontWeight: 'bold' }} className="btn btn-secondary btn-sm" >Increment</button>
             </React.Fragment>
         );
     }
