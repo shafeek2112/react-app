ab7ce1b244cb99afd1bfde28e3245bd8c6d7bbba
Shafeek <mohammedshafeek2112@gmail.com>
2020-08-23 14:03:50 +0800

    Intial setup and create simple component

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+
+class Counter extends Component {
+
+    render() {
+        return <h1>Hello Shafeek</h1>;
+    }
+}
+
+export default Counter;
