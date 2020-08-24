4dd33eeeb696cae4eba28773ab20ca5f6a86f5a9
Shafeek <mohammedshafeek2112@gmail.com>
2020-08-25 06:56:48 +0800

    Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ import ReactDom from 'react-dom';
 class Counter extends Component {
 
     state = {
-        count : 0
+        count : 0,
+        tags  : ['tag1','tag2', 'tag3']
     }
 
     // In the style property, should use the camelcase for attribute.
@@ -19,6 +20,10 @@ class Counter extends Component {
             <React.Fragment>
                 <span style={this.style} className={this.getBadgeClassNames()} >{ this.countFormat() }</span> 
                 <button style={{ fontWeight: 'bold' }} className="btn btn-secondary btn-sm" >Increment</button>
+                <ul>
+                    {/* Below li tag we used 'key' because in react each iterative line need unique key, then only react can update that particular element if any changes in DOM */}
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+                </ul>
             </React.Fragment>
         );
     }
