16983bc42df52e98f7de970e5bc1b1f7049f019c
Shafeek <mohammedshafeek2112@gmail.com>
2020-08-25 07:11:57 +0800

    Conditional Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,20 @@ class Counter extends Component {
 
     state = {
         count : 0,
-        tags  : ['tag1','tag2', 'tag3']
+        tags  : ['tag1','tag2', 'tag3'],
     }
 
     // In the style property, should use the camelcase for attribute.
     //Can apply style using two method, one is here or inline.
     style = {
         fontWeight : 'bold',
+    }
+
+    rederTagList() {
+
+        if(this.state.tags.length  === 0)  return <p>There are no tags</p>
+
+        return this.state.tags.map(tag => <li key={tag}>{tag}</li>) 
     }
 
     render() {
@@ -22,6 +29,18 @@ class Counter extends Component {
                 <button style={{ fontWeight: 'bold' }} className="btn btn-secondary btn-sm" >Increment</button>
                 <ul>
                     {/* Below li tag we used 'key' because in react each iterative line need unique key, then only react can update that particular element if any changes in DOM */}
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+                </ul>
+                
+                {/* Conditional rendering has two methods */}
+                {/* method 1 */}
+                <ul>
+                    { this.rederTagList() }
+                </ul>
+               
+                {/* method 2 */}
+                <ul>
+                    { this.state.tags.length  === 0 && <p>There are no tags from method 2</p> }
                     { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
                 </ul>
             </React.Fragment>
