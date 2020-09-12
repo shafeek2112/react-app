7c09e6b918f031cbfd6050e29bd6161da4f712ce
Shafeek <mohammedshfeek2112@gmail.com>
2020-09-12 12:39:40 +0800

    Single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,38 +3,10 @@ import ReactDom from 'react-dom';
 
 class Counter extends Component {
 
-    state = {
-        value : this.props.value,
-        tags  : ['tag1','tag2', 'tag3'],
-    }
-
     // In the style property, should use the camelcase for attribute.
     //Can apply style using two method, one is here or inline.
     style = {
         fontWeight : 'bold',
-    }
-
-    //constructor for given this to this 'handleIncrement' function.
-    /*constructor() {
-
-        super();
-        // console.log('constructor this -> ', this)
-        this.handleIncrement = this.handleIncrement.bind(this) // 1st method.
-    } */
-
-    //***********************  'this' keyword wont be available inside the normal function, even that function inside the class. So they  are two ways to do this.
-    //*********************** One is use the constructor and the bind method.
-    //*********************** Other one is change the 'handleIncrement' below method to arrow function.
-    /*handleIncrement() {
-
-        console.log("Increment Clicked ", this)
-    }*/
-
-    //2nd method - use arrow function
-    handleIncrement = (product) => {
-
-        //To change the state property we have to use setState function.
-        this.setState({ value: this.state.value + 1 });
     }
 
     render() {
@@ -45,24 +17,24 @@ class Counter extends Component {
                 <span style={this.style} className={this.getBadgeClassNames()} >{ this.countFormat() }</span> 
                 
                 {/* Beloe on onClick = we are passing the simple arrow function to handole the parameter, otherwise we cannot send the parameter to 'handleIncrement' function  */}
-                <button style={{ fontWeight: 'bold' }} onClick={() => this.handleIncrement({id : 1})} className="btn btn-secondary btn-sm" >Increment</button>
+                <button style={{ fontWeight: 'bold' }} onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm" >Increment</button>
 
                 {/* React RULE - For modifying the state like delete, add, edit - should be done at the componenet where this state is presented. So below we raise the 
                     delete event and then Counters component will handle this event  
                 */} 
-                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
 
     getBadgeClassNames() {
         let classes = "badge m-2 ";
-        classes += (this.state.value === 0) ? 'badge-warning' : 'badge-primary';
+        classes += (this.props.counter.value === 0) ? 'badge-warning' : 'badge-primary';
         return classes;
     }
 
     countFormat() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return (value === 0) ? 'Zero' : value;
     }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,23 +14,44 @@ class Counters extends Component {
         ]
     }
 
+    handleIncrement = counterParam => {
+
+        // In we clone the state.counter obj  into another and then modify it will directly change the state obj,
+        // so to avoid this, first clone the state ob and then clone the particular counter obj which need to
+        // be changed in the same index, then it wont change the state directly.
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counterParam)
+        counters[index] = {...counterParam} // This line is used to create different counters obj from state.
+        counters[index].value++;
+        console.log(counters, this.state.counters);
+        this.setState({ counters });
+    }
+
     handleDelete = (counterId) => {
         // console.log('Event handler called.',counter)
         const counters = this.state.counters.filter( (counter) =>  counter.id !== counterId);
         this.setState({counters});
     }
 
+    handleReset = () => {
+        const counters = this.state.counters.map( counter => {
+            counter.value = 0;
+            return counter;
+        });
+        console.log(counters, this.state.counters);
+        this.setState({counters});
+    }
+
     render() {
-
-
         return (
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 { this.state.counters.map( counter =>
                     <Counter 
                         key={counter.id} 
-                        value={counter.value} 
-                        id={counter.id} 
-                        onDelete={this.handleDelete} 
+                        onDelete={this.handleDelete}
+                        onIncrement={this.handleIncrement}
+                        counter={counter}
                         selected='true' 
                     />  
                 )}
