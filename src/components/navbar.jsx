b9aea0e620cd3cbd917629ebb992ead5eb4ad251
Shafeek <mohammedshfeek2112@gmail.com>
2020-09-13 09:18:55 +0800

    Argunment n object destructing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
-class NavBar extends Component {
-
-    render() {
-
-        return(
-            <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">Navbar
-                    <span className="badge badge-pill m-1 badge-secondary">{this.props.count}</span>
-                </span>
-            </nav>
-        );
-    }
+//Instead of class we can also use the stateless functional component & we using argument destructing
+const NavBar = ({count}) => {
+    return(
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand mb-0 h1">Navbar
+                <span className="badge badge-pill m-1 badge-secondary">{count}</span>
+            </span>
+        </nav>
+    );
 }
 
 export default NavBar;
